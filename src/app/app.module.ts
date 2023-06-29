b020b60f172c6baeaeff7e1c046286f27bb7b21b
eViloria <evanvineevanvine@gmail.com>
2023-06-29 06:54:17 -0500

    Ejercicio donde uso angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { ContactosComponent } from './contactos/contactos.component';
+import { RouterModule, Routes } from '@angular/router';
+const Rutas: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: 'QuienesSomos', component: QuienesSomosComponent },
+  { path: 'Contactos', component: ContactosComponent },
+  { path: 'Proyectos', component: ProyectosComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuienesSomosComponent,
+    ProyectosComponent,
+    ContactosComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(Rutas)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
